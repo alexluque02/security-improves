7614fe54a170ec0219198a693f19a98ffe167c55
Tomasdelpinorep <tomasdelpino28@gmail.com>
2023-12-11 20:57:26 +0100

    baeldung example implemented

---
+++ src/main/java/net/openwebinars/springboot/restjwt/security/notifyByLocationAndDevice/DeviceMetadataRepository.java
@@ -0,0 +1,10 @@
+package net.openwebinars.springboot.restjwt.security.notifyByLocationAndDevice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface DeviceMetadataRepository extends JpaRepository<DeviceMetadata, UUID> {
+    List<DeviceMetadata> findByUserId(UUID userId);
+}
